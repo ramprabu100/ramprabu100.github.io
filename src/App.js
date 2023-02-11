a705489cd9f592338c102edf995158ac33be3a53
Ramachandraprabhu T <rt@demandbase.com>
2023-02-11 22:09:46 +0530

    app js modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Introduction from "./components/Introduction";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import AboutMe from "./components/AboutMe";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    activeSection: "Introduction",
+  };
+
+  handleNavClick = (section) => {
+    this.setState({
+      activeSection: section,
+    });
+  };
+
+  render() {
+    const { activeSection } = this.state;
+    return (
+      <div className="App">
+        <div className="nav-bar">
+          <div
+            className={
+              activeSection === "Introduction" ? "active-nav-item" : ""
+            }
+            onClick={() => this.handleNavClick("Introduction")}
+          >
+            Introduction
+          </div>
+          <div
+            className={activeSection === "Skills" ? "active-nav-item" : ""}
+            onClick={() => this.handleNavClick("Skills")}
+          >
+            Skills
+          </div>
+          <div
+            className={activeSection === "Experience" ? "active-nav-item" : ""}
+            onClick={() => this.handleNavClick("Experience")}
+          >
+            Experience
+          </div>
+          <div
+            className={activeSection === "Education" ? "active-nav-item" : ""}
+            onClick={() => this.handleNavClick("Education")}
+          >
+            Education
+          </div>
+          <div
+            className={activeSection === "AboutMe" ? "active-nav-item" : ""}
+            onClick={() => this.handleNavClick("AboutMe")}
+          >
+            About Me
+          </div>
+        </div>
+        <div className="content-container">
+          {activeSection === "Introduction" && <Introduction />}
+          {activeSection === "Skills" && <Skills />}
+          {activeSection === "Experience" && <Experience />}
+          {activeSection === "Education" && <Education />}
+          {activeSection === "AboutMe" && <AboutMe />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
